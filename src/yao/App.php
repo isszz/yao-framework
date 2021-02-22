e725179facf5fe5901da3b7c80d15e28ee7b46d8
chengyao <bigyao@139.com>
2021-02-22 21:07:38 +0800

    添加部分注释

--- src/yao/App.php
+++ src/yao/App.php
@@ -5,23 +5,22 @@ declare(strict_types=1);
 namespace Yao;
 
 use App\Http\Validate;
+use Yao\{Cache\Setter, Event\Event, View\Render};
 use Yao\Http\{Middleware, Request, Response, Route, Route\Alias, Session};
-use Yao\Cache\Setter;
-use Yao\Event\Event;
-use Yao\View\Render;
 
 /**
- * @property Request $request
- * @property Validate $validate
- * @property Env $env
- * @property Config $config
- * @property Render $view
- * @property Response $response
- * @property Session $session
- * @property Log $log
- * @property Cache $cache
- * @property Event $event
- * @property Middleware $middleware
+ * @property Request $request 请求实例
+ * @property Validate $validate 验证器实例
+ * @property Env $env 环境变量实例
+ * @property Config $config 配置类实例
+ * @property Render $view 视图渲染类实例
+ * @property Response $response 响应实例
+ * @property Session $session Session实例
+ * @property Log $log 日志类实例
+ * @property Route $route 路由实例
+ * @property Cache $cache 缓存类实例
+ * @property Event $event 事件实例
+ * @property Middleware $middleware 中间件实例
  * Class App
  * @package Yao
  */
@@ -54,17 +53,23 @@ class App extends Container
     {
         set_time_limit(30);
         function_exists('ini_set') && ini_set('memory_limit', '64M');
+        ob_start();
+        //注册错误和异常
         $this['error']->register();
+        //触发app_start事件
         $this['event']->trigger('app_start');
         //        ignore_user_abort(true);
-        ob_start();
+        //Session自动开启
         if ($this['config']->get('app.auto_start')) {
             isset($_SESSION) || session_start();
             // $this['session']->flashCheck();
         }
         date_default_timezone_set($this->config->get('app.default_timezone', 'PRC'));
+        //初始化绑定类
         $this->bind = array_merge((array)$this->config->get('app.alias'), $this->bind);
+        //注册路由
         $this['route']->register();
+        //发送响应
         return $this->response->data(
             $this['middleware']->make(function () {
                 return $this->route->dispatch();
