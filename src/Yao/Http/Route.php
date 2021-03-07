23a3c8c83a605fbf3a4b6e961bd153bd2cc74b63
chengyao <bigyao@139.com>
2021-03-07 18:24:23 +0800

    去掉了method

--- src/Yao/Http/Route.php
+++ src/Yao/Http/Route.php
@@ -217,7 +217,7 @@ class Route
      * 请求方法
      * @return $this
      */
-    public function cors($allowOrigin = '*', string $allowCredentials = 'true', string $allowHeaders = 'Origin,Content-Type,Accept,token,X-Requested-With', int $allowAge = 600, $methods = 'GET,POST,PUT,DELETE,PATCH,OPTIONS'): Route
+    public function cors($allowOrigin = '*', string $allowCredentials = 'true', string $allowHeaders = 'Origin,Content-Type,Accept,token,X-Requested-With', int $allowAge = 600): Route
     {
         if ($this->request->path() == $this->path || preg_match("#^{$this->path}$#iU", $this->request->path())) {
             $this->app[Cors::class]->set(['Access-Control-Allow-Origin:' . $allowOrigin, 'Access-Control-Allow-Methods:' . strtoupper($this->request->method()), 'Access-Control-Allow-Credentials:' . $allowCredentials, 'Access-Control-Allow-Headers:' . $allowHeaders, 'Access-Control-Max-Age:' . $allowAge]);
